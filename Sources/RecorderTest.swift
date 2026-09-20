38a44ba73af3b52983ea08f55c48be6c87ff3c2e
arvind-kandari <arvindkandari5500@gmail.com>
2026-09-20 22:18:13 +0530

    Update recorder test for new recording session

--- Sources/RecorderTest.swift
+++ Sources/RecorderTest.swift
@@ -5,36 +5,6 @@ final class RecorderTest {
     private let session = RecordingSession()
 
     func configure() {
-
-        session.onSensorReading = { reading in
-
-            print("""
-            Sensor:
-              Recording Time: \(reading.recordingTime)
-              Sensor Timestamp: \(reading.sensorTimestamp)
-
-              Acceleration:
-                X: \(reading.accelerationX)
-                Y: \(reading.accelerationY)
-                Z: \(reading.accelerationZ)
-
-              Gyroscope:
-                X: \(reading.rotationRateX)
-                Y: \(reading.rotationRateY)
-                Z: \(reading.rotationRateZ)
-
-              Magnetometer:
-                X: \(reading.magneticFieldX)
-                Y: \(reading.magneticFieldY)
-                Z: \(reading.magneticFieldZ)
-
-              Attitude:
-                Roll: \(reading.roll)
-                Pitch: \(reading.pitch)
-                Yaw: \(reading.yaw)
-            """)
-        }
-
         session.configure()
     }
 
@@ -43,8 +13,7 @@ final class RecorderTest {
     }
 
     func stop() {
-        session.stop {
-            print("Recording test finished")
-        }
+        session.stop()
+        print("Recording test finished")
     }
 }

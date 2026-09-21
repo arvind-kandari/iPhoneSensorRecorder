0b8e9e4dc1a09a5c96f5a03c064e14e4fbed547d
arvind-kandari <arvindkandari5500@gmail.com>
2026-09-21 11:25:52 +0530

    Fix fullscreen camera preview

--- Sources/CameraPreview.swift
+++ Sources/CameraPreview.swift
@@ -6,24 +6,53 @@ struct CameraPreview: UIViewRepresentable {
 
     func makeUIView(context: Context) -> PreviewView {
         let view = PreviewView()
+
         view.previewLayer.session = session
         view.previewLayer.videoGravity = .resizeAspectFill
+
         rotate(view.previewLayer.connection)
+
         return view
     }
 
-    func updateUIView(_ view: PreviewView, context: Context) {
+    func updateUIView(
+        _ view: PreviewView,
+        context: Context
+    ) {
         view.previewLayer.session = session
+        view.previewLayer.videoGravity = .resizeAspectFill
+
         rotate(view.previewLayer.connection)
     }
 
-    private func rotate(_ connection: AVCaptureConnection?) {
-        guard let connection, connection.isVideoRotationAngleSupported(90) else { return }
+    private func rotate(
+        _ connection: AVCaptureConnection?
+    ) {
+        guard
+            let connection,
+            connection.isVideoRotationAngleSupported(90)
+        else {
+            return
+        }
+
         connection.videoRotationAngle = 90
     }
 }
 
 final class PreviewView: UIView {
-    override class var layerClass: AnyClass { AVCaptureVideoPreviewLayer.self }
-    var previewLayer: AVCaptureVideoPreviewLayer { layer as! AVCaptureVideoPreviewLayer }
+
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+
+    var previewLayer: AVCaptureVideoPreviewLayer {
+        layer as! AVCaptureVideoPreviewLayer
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        previewLayer.frame = bounds
+        previewLayer.videoGravity = .resizeAspectFill
+    }
 }
